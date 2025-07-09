1d6892b3c2315d2040117ded2ec89960589e84de
Hoan.Hoang <hoan.hoang@asnet.com.vn>
2025-07-09 17:02:26 +0700

    feat: preconfig for app

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+// prettier.config.js
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 100,
+  tabWidth: 2,
+  plugins: ['prettier-plugin-tailwindcss'],
+};
