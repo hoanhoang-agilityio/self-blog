6afb57b7e4528b3b7c0205d324785a85b75b677c
Hoan.Hoang <hoan.hoang@asnet.com.vn>
2025-07-13 16:35:25 +0700

    feat: init login form

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,3 +1,14 @@
-export default function login() {
-  return <div>Login</div>;
+import LoginForm from '@/components/ui/login-form';
+import { Suspense } from 'react';
+
+export default function LoginPage() {
+  return (
+    <main className="mt-10 flex items-center justify-center">
+      <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
+        <Suspense>
+          <LoginForm />
+        </Suspense>
+      </div>
+    </main>
+  );
 }
